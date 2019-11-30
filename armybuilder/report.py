19b0f58d6da807d870ffc3d36dbd5467fa902563
Michael Sobczak <michaelsobczak54@gmail.com>
2019-11-30 00:37:05 -0500

    move to db model

--- armybuilder/report.py
+++ armybuilder/report.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from typing import Dict, Any
+from sqlalchemy.sql.expression import func
 import os
 from . import app,db
 from .models import *
@@ -10,78 +11,30 @@ def render_report_template(template_name: str, context: Dict[str, Any]) -> str:
             template_name, **context
         )
 
-def generate_report(output_dir: str):
+def generate_report(output_dir: str, roster_id: int):
 
     # make the output dir if it exists
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    # generate the a dictionary of document name to template context
+    dummy_roster = db.session.query(Roster).get(roster_id)
+    def dummy_tactic():
+        return db.session.query(Tactic).order_by(func.random()).first()
     
-    dummy_ability = {
-        'id' : 0,
-        'name' : 'ABILITY_NAME',
-        'text' : 'ABILITY_TEXT'
-    }
+    def dummy_model():
+        return db.session.query(Figure).order_by(func.random()).first()
 
-    dummy_wargear = {
-        'name' : 'WARGEAR_NAME',
-        'range' : 'RANGE',
-        'type' : 'HEAVY TYPE 6',
-        'strength' : 'STRENGTH',
-        'ap' : '-AP',
-        'damage' : "DAMAGE",
-        'abilities' : [dummy_ability]
-    }
-
-    dummy_tactic = {
-        'id' : 0,
-        'name' : 'TACTIC_NAME',
-        'cost' : 2,
-        'text' : 'TACTIC_TEXT',
-        'faction' : 'TACTIC_FACTION'
-    }
-
-    dummy_specialization = {
-        'id' : 0,
-        'name' : 'SPECIALIZATION_NAME',
-        'tactic' : dummy_tactic,
-        'passive' : 'SPECIALIST_PASSIVE'
-    }
-
-    dummy_model = {
-        'id' : 0,
-        'name' : 'MODEL NAME',
-        'model_type' : 'MODEL_TYPE',
-        'wargear' : [dummy_wargear, dummy_wargear],
-        'specialization' : dummy_specialization,
-        'points' : 10,
-        'move' : 5,
-        'weapon_skill' : 3,
-        'balistic_skill' : 5,
-        'strength' : 4,
-        'toughness' : 4,
-        'wounds' : 1,
-        'attacks' : 2,
-        'leadership' : 6,
-        'save' : 6,
-        'abilities' : [dummy_ability],
-        'faction' : 'MODEL_FACTION',
-        'keywords' : ["Keyword1", "Keyword2"]
-
-    }
-    dummy_roster = db.session.query(Roster).all()[1]
 
     document_context_map = {
         'cheatsheet': {
             'cheatsheetRows': ['first', 'second', 'third'],
-            'common_tactics' : [dummy_tactic, dummy_tactic,dummy_tactic, dummy_tactic,dummy_tactic, dummy_tactic,dummy_tactic],
-            'faction_tactics' : [dummy_tactic, dummy_tactic,dummy_tactic, dummy_tactic,dummy_tactic, dummy_tactic,dummy_tactic, dummy_tactic,dummy_tactic, dummy_tactic,dummy_tactic, dummy_tactic],
-            'specialist_tactics' : [dummy_tactic, dummy_tactic,dummy_tactic, dummy_tactic],
-            'unique_models' : [dummy_model,dummy_model,dummy_model]
+            'common_tactics' : [dummy_tactic(), dummy_tactic(),dummy_tactic(), dummy_tactic(),dummy_tactic(), dummy_tactic(),dummy_tactic()],
+            'faction_tactics' : [dummy_tactic(), dummy_tactic(),dummy_tactic(), dummy_tactic(),dummy_tactic(), dummy_tactic(),dummy_tactic(), dummy_tactic(),dummy_tactic(), dummy_tactic(),dummy_tactic(), dummy_tactic()],
+            'specialist_tactics' : [dummy_tactic(), dummy_tactic(),dummy_tactic(), dummy_tactic()],
+            'unique_models' : [dummy_model(),dummy_model(),dummy_model()]
         },
         'datacards': {
-            'models' : [dummy_model, dummy_model, dummy_model],
+            'models' : [dummy_model(), dummy_model(), dummy_model()],
         },
         'roster': {
             'roster' : dummy_roster
--- scripts/generate_report.py
+++ scripts/generate_report.py
@@ -2,16 +2,27 @@ import argparse
 import os
 import sys
 
+from armybuilder import db
+from armybuilder.models import Roster
 from armybuilder.report import generate_report
 
 def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('--output-dir', help='path to output generated', default=os.path.join(os.getcwd(), 'output'))
+    parser.add_argument('--list-rosters', action='store_true', help='list available rosters')
+    parser.add_argument('roster_id', help='roster id number')
+
     args = parser.parse_args(argv)
 
+    if args.list_rosters:
+        rosters = db.session.query(Roster).all()
+        print('\n\t' + '\n\t'.join([ f'{r.id}: {r.name}' for r in rosters]))
+        return 1
+
     print(f'Saving file to {args.output_dir}...')
-    generate_report(args.output_dir)
+    generate_report(args.output_dir, args.roster_id)
     print(f'Done generating report!')
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
