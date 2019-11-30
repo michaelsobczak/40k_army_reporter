4716a02154eefb0d6a8975b7d3d7b1fa0ad74bd1
Michael Sobczak <michaelsobczak54@gmail.com>
2019-11-30 16:19:25 -0500

    added environment checking and creation to the script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -7,6 +7,8 @@ from subprocess import Popen,PIPE,STDOUT
 import logging
 
 DEFAULT_APP_NAME = 'armybuilder'
+DEFAULT_ENV_NAME = 'armybuilder-production'
+DEFAULT_STACK_NAME = '64bit Amazon Linux 2018.03 v2.14.0 running Docker 18.09.9-ce'
 
 def run_aws_cmd(service: str, *args) -> Dict[Any, Any]:
     cmd_args = ["aws", service] + list(args)
@@ -35,17 +37,35 @@ def app_exists(appname: str):
             return True
     return False
 
+def env_exists(appname: str, envname: str) -> bool:
+    env_data = run_ebs_cmd('describe-environments', '--application-name', appname)
+    if not env_data:
+        logging.error(f'unable to load existing environment data')
+
+    for env in env_data['Environments']:
+        if env['EnvironmentName'] == envname:
+            logging.info(f'Found existing environment {envname}')
+            return True
+    return False
+
 def create_app(appname: str):
     logging.info("Creating new elasticbeanstalk application...")
     new_app = run_ebs_cmd('create-application', '--application-name', appname)
     logging.info(f"Successfully created new application {appname}!")
     logging.debug(json.dumps(new_app, indent=4))
 
+def create_env(appname: str, envname: str, stackname: str):
+    logging.info(f"Creating environment {envname} in {appname}")
+    new_env = run_ebs_cmd('create-environment', '--application-name', appname, '--environment-name', envname, '--solution-stack-name', stackname)
+
+
 def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('--verbose', action='store_true')
     parser.add_argument('--logfile', default=os.path.join(os.getcwd(), 'deploy.log'))
     parser.add_argument('--application-name', default=DEFAULT_APP_NAME)
+    parser.add_argument('--environment-name', default=DEFAULT_ENV_NAME)
+    parser.add_argument('--stack-name', default=DEFAULT_STACK_NAME)
     args = parser.parse_args(argv if argv else sys.argv[1:])
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
@@ -55,7 +75,10 @@ def main(argv=None):
     if not app_exists(args.application_name):
         create_app(args.application_name)
     
+    if not env_exists(args.application_name, args.environment_name):
+        create_env(args.application_name, args.environment_name, args.stack_name)
 
+    
 
 if __name__ == '__main__':
     sys.exit(main())
