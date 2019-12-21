5a47382d1f325a6ad322980b7f4546443a0443cc
juliantoker <juliantoker@gmail.com>
2019-12-21 13:42:17 -0500

    got roster tempate working again datacard and cheatcheat broken still

--- armybuilder/report.py
+++ armybuilder/report.py
@@ -20,46 +20,46 @@ def generate_report(output_dir: str, roster_id: int) -> Dict[str, str]:
 
     roster = db.session.query(Roster).get(roster_id)
     print(roster)
-    def dummy_tactic():
-        return db.session.query(Tactic).order_by(func.random()).first()
+    # def dummy_tactic():
+    #     return db.session.query(Tactic).order_by(func.random()).first()
     
-    def dummy_model():
-        return db.session.query(Figure).order_by(func.random()).first()
+    # def dummy_model():
+    #     return db.session.query(Figure).order_by(func.random()).first()
 
-    common_keyword = db.session.query(Keyword).filter_by(label='Common').first()
-    if not common_keyword:
-        return
-    common_faction = db.session.query(Faction).filter_by(keyword_id=common_keyword.id).first()
-    if not common_faction:
-        print(f'No Common Faction found, exiting')
+    # common_keyword = db.session.query(Keyword).filter_by(label='Common').first()
+    # if not common_keyword:
+    #     return
+    # common_faction = db.session.query(Faction).filter_by(keyword_id=common_keyword.id).first()
+    # if not common_faction:
+    #     print(f'No Common Faction found, exiting')
     
-    common_tactics = db.session.query(Tactic).filter(Tactic.factions.any(Faction.id == common_faction.id)).all()
+    # common_tactics = db.session.query(Tactic).filter(Tactic.factions.any(Faction.id == common_faction.id)).all()
 
 
-    # loop through factions and find the tactics that match
-    faction_tactics = []
-    for f in roster.factions:
-        faction_tactics += db.session.query(Tactic).filter(Tactic.factions.any(Faction.id == f.id)).all()
+    # # loop through factions and find the tactics that match
+    # faction_tactics = []
+    # for f in roster.factions:
+    #     faction_tactics += db.session.query(Tactic).filter(Tactic.factions.any(Faction.id == f.id)).all()
     
-    specialist_tactics = []
-    model_id_map = {}
-    for roster_entry in roster.entries:
-        if roster_entry.specialization and roster_entry.specialization.tactic:
-            specialist_tactics.append(roster_entry.specialization.tactic)
-        model_id_map[roster_entry.figure.id] = roster_entry.figure
+    # specialist_tactics = []
+    # model_id_map = {}
+    # for roster_entry in roster.entries:
+    #     if roster_entry.specialization and roster_entry.specialization.tactic:
+    #         specialist_tactics.append(roster_entry.specialization.tactic)
+    #     model_id_map[roster_entry.figure.id] = roster_entry.figure
     
 
     document_context_map = {
-        'cheatsheet': {
-            'roster' : roster,
-            'common_tactics' : common_tactics,
-            'faction_tactics' : faction_tactics,
-            'specialist_tactics' : specialist_tactics,
-            'unique_models' : list(model_id_map.values())
-        },
-        'datacards': {
-            'roster' : roster,
-        },
+        # 'cheatsheet': {
+        #     'roster' : roster,
+        #     'common_tactics' : common_tactics,
+        #     'faction_tactics' : faction_tactics,
+        #     'specialist_tactics' : specialist_tactics,
+        #     'unique_models' : list(model_id_map.values())
+        # },
+        # 'datacards': {
+        #     'roster' : roster,
+        # },
         'roster': {
             'roster' : roster
         }
